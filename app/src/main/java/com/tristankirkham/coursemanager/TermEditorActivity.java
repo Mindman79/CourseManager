4924cca3ac30fceba901b05870112f82b296d3b6
Mindman79 <39894662+Mindman79@users.noreply.github.com>
2019-07-14 14:58:43 -0600

    Still working on inserting dates

--- app/src/main/java/com/tristankirkham/coursemanager/TermEditorActivity.java
+++ app/src/main/java/com/tristankirkham/coursemanager/TermEditorActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
